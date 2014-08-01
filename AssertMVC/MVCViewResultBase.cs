d4affba27f36fc7f69a452ed0f00fb131565652e
Vincent <vincentredrum@gmail.com>
2014-08-01 09:15:31 +0100

    Squashed '3rdParty/' content from commit 5d2a401
    git-subtree-dir: 3rdParty
    git-subtree-split: 5d2a40172ab5a9579404868ec599915ceeb2d207

---
+++ AssertMVC/MVCViewResultBase.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Web.Mvc;
+
+namespace AssertMVC
+{
+    public static class MVCViewResultBase
+    {
+        public static void WithName(this ViewResultBase result, string expectedName)
+        {
+            Assert.AreEqual(expectedName, result.ViewName);
+        }
+
+        public static void WithName(this ViewResultBase result, Func<string, bool> evaluationFunction)
+        {
+            Assert.IsTrue(evaluationFunction(result.ViewName));
+        }
+
+        public static void Default(this ViewResultBase result)
+        {
+            Assert.AreEqual(string.Empty, result.ViewName);
+        }
+
+        public static MVCModel WithModel(this ViewResultBase result)
+        {
+            Assert.IsNotNull(result.Model);
+            return new MVCModel(result.Model);
+        }
+
+        public static MVCViewBag WithViewBag(this ViewResultBase result)
+        {
+            Assert.IsNotNull(result.ViewBag);
+            return new MVCViewBag(result.ViewData);
+        }
+
+        public static MVCViewData WithViewData(this ViewResultBase result)
+        {
+            Assert.IsNotNull(result.ViewBag);
+            return new MVCViewData(result.ViewData);
+        }
+    }
+}
