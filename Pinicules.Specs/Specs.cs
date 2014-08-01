d7ea0805a6cb2181bde5e21ebd07accd9358dbdf
Vincent <vincentredrum@gmail.com>
2014-08-01 09:13:03 +0100

    Added first spec

---
+++ Pinicules.Specs/Specs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pinicules.Specs
+{
+    [TestClass]
+    public class Specs
+    {
+        [TestMethod]
+        public void If_I_Have_Ten_Movies_In_DB_Controller_Returns_A_Model_With_Ten_Items()
+        {
+            IMoviesRepository moviesRepository = new InMemoryMoviesRepository();
+            IMoviesService moviesService = new moviesService(moviesRepository);
+            var moviesController = new MoviesController(moviesService);
+
+            ActionResult result = moviesController.Index();
+
+        }
+    }
+}
