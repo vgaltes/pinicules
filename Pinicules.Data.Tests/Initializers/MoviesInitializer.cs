f8579539faf03fbe1bf633636568f2d10e9fdbd8
Vincent <vincentredrum@gmail.com>
2014-08-06 15:31:38 +0100

    Retrieving8ElementsShouldReturnTheFirst8Elements -> red

---
+++ Pinicules.Data.Tests/Initializers/MoviesInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pinicules.Data.Repositories;
+
+namespace Pinicules.Data.Tests.Initializers
+{
+    public class MoviesInitializer : DropCreateDatabaseAlways<MoviesRepository>
+    {
+        protected override void Seed(MoviesRepository context)
+        {
+            base.Seed(context);
+        }
+    }
+}
---
+++ Pinicules.Infrastructure/Repositories/MoviesRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pinicules.Domain.DTOs;
+using Pinicules.Domain.Repositories;
+
+namespace Pinicules.Data.Repositories
+{
+    public class MoviesRepository : IMoviesRepository
+    {
+        public List<MovieDTO> GetMovies(string searchTerm, int numItems, int page, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(int movieId, string title)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
