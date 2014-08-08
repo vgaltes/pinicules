4f9f5f60f8fd67e89395aada0b04832588edabf8
Vincent <vincentredrum@gmail.com>
2014-08-08 16:07:22 +0100

    Restrict access to application.

---
+++ Pinicules.Presentation/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pinicules.Presentation.Controllers
+{
+    [AllowAnonymous]
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Pinicules.Presentation/Global.asax.cs
+++ Pinicules.Presentation/Global.asax.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Pinicules.Presentation
@@ -14,6 +10,7 @@ namespace Pinicules.Presentation
             AreaRegistration.RegisterAllAreas();
             UnityConfig.RegisterComponents();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
         }
     }
 }
