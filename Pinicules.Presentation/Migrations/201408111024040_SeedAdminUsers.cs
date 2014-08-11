1a14e9ab265debe44049a46a36eefc1769b08f4f
Vincent <vincentredrum@gmail.com>
2014-08-11 11:41:17 +0100

    Added roles

---
+++ Pinicules.Presentation/Migrations/201408111024040_SeedAdminUsers.cs
@@ -0,0 +1,16 @@
+namespace Pinicules.Presentation.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedAdminUsers : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- Pinicules.Presentation/Migrations/Configuration.cs
+++ Pinicules.Presentation/Migrations/Configuration.cs
@@ -1,11 +1,15 @@
 namespace Pinicules.Presentation.Migrations
 {
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Pinicules.Presentation.Identity;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Pinicules.Presentation.Identity.AppDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<AppDbContext>
     {
         public Configuration()
         {
@@ -13,20 +17,35 @@ namespace Pinicules.Presentation.Migrations
             ContextKey = "Pinicules.Presentation.Identity.AppDbContext";
         }
 
-        protected override void Seed(Pinicules.Presentation.Identity.AppDbContext context)
+        protected override void Seed(AppDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            //context.Roles.AddOrUpdate(new IdentityRole[] { new IdentityRole("Admin") });
+            CreateRole("Admin");
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            AddAdminUser(context, "vicente", "peliculas");
+            AddAdminUser(context, "vincent", "pelicules");
+        }
+
+        private void CreateRole(string roleName)
+        {
+            var rm = new RoleManager<IdentityRole>(
+            new RoleStore<IdentityRole>(new AppDbContext()));
+            var idResult = rm.Create(new IdentityRole(roleName));
+        }
+
+        private static void AddAdminUser(AppDbContext context, string userName, string password)
+        {
+            var userStore = new UserStore<AppUser>(
+                    new AppDbContext());
+            var manager = new UserManager<AppUser>(
+                userStore);
+            var user = manager.FindByName(userName);
+            if ( user == null ){
+                manager.Create(new AppUser { UserName = userName }, password);
+            }
+
+            user = manager.FindByName(userName);
+            manager.AddToRole(user.Id, "Admin");
         }
     }
 }
